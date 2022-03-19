e8e792a4799b174225cfefc247df617810550c1d
vanessaxuuan <64406694+vanessaxuuan@users.noreply.github.com>
2022-03-20 04:16:59 +0800

    Update Student.java

--- src/main/java/seedu/address/model/student/Student.java
+++ src/main/java/seedu/address/model/student/Student.java
@@ -92,8 +92,8 @@ public class Student {
      * @return if any tutorial group exists under this Student
      */
     public boolean tutorialGroupExists(Set<TutorialGroup> toCheck) {
-        for(TutorialGroup tg : toCheck) {
-            if(tutorialGroups.contains(tg)) {
+        for (TutorialGroup tg : toCheck) {
+            if (tutorialGroups.contains(tg)) {
                 return true;
             }
         }
