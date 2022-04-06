28c5b74d88256afc89571a3042e7739214f3f6e0
jaysmyname <jaysmyname19@gmail.com>
2022-04-06 16:09:56 +0800

    Update name tests

--- src/test/java/seedu/address/model/student/NameTest.java
+++ src/test/java/seedu/address/model/student/NameTest.java
@@ -27,14 +27,23 @@ public class NameTest {
         // invalid name
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
-        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("12345")); // numbers only
+        assertFalse(Name.isValidName("peter the 2nd")); // alphanumeric characters
+        assertFalse(Name.isValidName("^")); // only non-alphabetical characters
+        assertFalse(Name.isValidName("peter*")); // contains unaccepted characters
+        assertFalse(Name.isValidName("peter-")); // hyphen at the end
+        assertFalse(Name.isValidName("-peter")); // hyphen at the start
+        assertFalse(Name.isValidName("peter\'")); // apostrophe at the end
+        assertFalse(Name.isValidName("\'peter")); // apostrophe at the start
+        assertFalse(Name.isValidName("peter--jack")); // consecutive hyphens
+        assertFalse(Name.isValidName("peter   jack")); // consecutive spaces
+        assertFalse(Name.isValidName("peter\'\'jack")); // consecutive apostrophes
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
-        assertTrue(Name.isValidName("12345")); // numbers only
-        assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
-        assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("Max-Ernest")); // name with hyphen
+        assertTrue(Name.isValidName("Jeanne d'Arc")); // name with apostrophe
+        assertTrue(Name.isValidName("Johnson-Johnson d'Arby")); // name with all hyphen, space and apostrophe
     }
 }
