58997d667136fa1fffc44ea67f94946e5e93c279
jaysmyname <jaysmyname19@gmail.com>
2022-04-06 16:09:42 +0800

    Update name regex

--- src/main/java/seedu/address/model/student/Name.java
+++ src/main/java/seedu/address/model/student/Name.java
@@ -10,13 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain letters, and can contain a single space, apostrophe or hyphen surrounded by "
+                    + "letters. Names cannot be blank and cannot be longer than 100 characters in total.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[A-Za-z]([ '-]?[A-Za-z])*";
 
     public final String fullName;
 
@@ -27,15 +28,15 @@ public class Name {
      */
     public Name(String name) {
         requireNonNull(name);
-        checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
-        fullName = name;
+        checkArgument(isValidName(name.trim()), MESSAGE_CONSTRAINTS);
+        fullName = name.trim();
     }
 
     /**
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) && test.length() <= 100;
     }
 
 
