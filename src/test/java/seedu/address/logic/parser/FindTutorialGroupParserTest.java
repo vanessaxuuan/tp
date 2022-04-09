5e080b88df069c6dfcdafcce7bea8b436bb06184
vanessaxuuan <64406694+vanessaxuuan@users.noreply.github.com>
2022-04-09 14:17:09 +0800

    Update tutorialGroupParserTest

--- src/test/java/seedu/address/logic/parser/FindTutorialGroupParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindTutorialGroupParserTest.java
@@ -19,14 +19,23 @@ public class FindTutorialGroupParserTest {
     }
 
     @Test
+    public void parse_invalidArgs_throwsPArseException() {
+        // no whitespace character to separate module and tutorial group
+        assertParseFailure(parser, "cs2100", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindTutorialGroupCommand.MESSAGE_USAGE));
+    }
+
+    @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindTutorialGroupCommand expectedFindCommand =
                 new FindTutorialGroupCommand(new TutorialGroupKeywordsPredicate("CS2101 G08"));
         assertParseSuccess(parser, "CS2101 G08", expectedFindCommand);
 
-        // multiple whitespaces between keywords
+        // with leading and trailing whitespaces
         assertParseSuccess(parser, " \n CS2101 G08 \n", expectedFindCommand);
-    }
 
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, "CS2101 \n G08", expectedFindCommand);
+    }
 }
