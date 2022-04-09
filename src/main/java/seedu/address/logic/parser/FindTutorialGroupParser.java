a28326d63446c153963ab7d9e1517252f8d4d925
vanessaxuuan <64406694+vanessaxuuan@users.noreply.github.com>
2022-04-09 14:21:23 +0800

    Checkstyle

--- src/main/java/seedu/address/logic/parser/FindTutorialGroupParser.java
+++ src/main/java/seedu/address/logic/parser/FindTutorialGroupParser.java
@@ -2,9 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
-import java.util.List;
-
 import seedu.address.logic.commands.FindTutorialGroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tutorialgroup.TutorialGroupKeywordsPredicate;
